ea1afc308301a19adf1b6a74f7d36f00aa2eb278
Arman Mazdaee <arman.maz1373@gmail.com>
2015-01-24 21:32:41 +0330

    Add magit package

--- global-packages.el
+++ global-packages.el
@@ -9,3 +9,9 @@
 (add-hook 'after-my-packages-init-hook
           (lambda ()
             (global-set-key (kbd "C-c SPC") 'ace-jump-mode)))
+
+;;a front end, or “porcelain” for the Git version control system
+(add-to-list 'my-packages-list 'magit)
+(add-hook 'after-my-packages-init-hook
+          (lambda ()
+            (global-set-key (kbd "C-x C-z") 'magit-status)))
