3091c16793c515b540bc934267fd6f84df96e02d
Arman Mazdaee <arman.maz1373@gmail.com>
2015-01-24 21:33:29 +0330

    Add smex and ido-mode

--- global-packages.el
+++ global-packages.el
@@ -15,3 +15,21 @@
 (add-hook 'after-my-packages-init-hook
           (lambda ()
             (global-set-key (kbd "C-x C-z") 'magit-status)))
+
+;;ido-mode
+(add-to-list 'my-packages-list 'flx-ido)
+(add-hook 'after-my-packages-init-hook
+          (lambda ()
+            (ido-mode 1)
+            (ido-everywhere 1)
+            (flx-ido-mode 1)
+            (setq ido-enable-flex-matching t)
+            (setq ido-use-faces nil)
+            (setq ido-save-directory-list-file "~/.emacs.d/ido")
+            (setq ido-use-filename-at-point 'guess)))
+
+;;a M-x enhancement for Emacs. Built on top of Ido
+(add-to-list 'my-packages-list 'smex)
+(add-hook 'after-my-packages-init-hook
+          (lambda ()
+            (global-set-key (kbd "M-x") 'smex)))
