a54c43a3b198122eee52e4fca2d369c30130983d
Arman Mazdaee <arman.maz1373@gmail.com>
2015-01-27 10:14:46 +0330

    Add .ejs to web-mode

--- web.el
+++ web.el
@@ -8,6 +8,7 @@
             (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
             (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
             (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+            (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
             (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))))
 
 (add-to-list 'my-packages-list 'emmet-mode)
