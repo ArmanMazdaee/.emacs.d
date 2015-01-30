105f37e0d4c5264105507459dcdae77a5f3409e7
Arman Mazdaee <arman.maz1373@gmail.com>
2015-01-30 12:43:11 +0330

    add a presonal recipe for auto-complete

--- package-recipes.el
+++ package-recipes.el
@@ -6,4 +6,9 @@
 ;; 		     :before (message "before installing sample package")
 ;; 		     :after (message "after installing sample package")))
 
-
+(add-to-list 'my-packages-recipes
+             '(:name auto-complete
+                     :after (add-hook 'auto-complete-mode-hook
+                                      (lambda ()
+                                        (ac-set-trigger-key "TAB")
+                                        (setq ac-auto-start nil)))))
