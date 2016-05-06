65d32807d1f492579ab07cb33ce92f7f26bbd080
Arman Mazdaee <arman.maz1373@gmail.com>
2016-05-06 10:36:41 +0430

    Fix avy package recipe bug

--- package-recipes/avy-recipe.el
+++ package-recipes/avy-recipe.el
@@ -8,12 +8,12 @@
 (defun my/avy-jump (arg)
   "custom jump for avy that acts like this:
         my/avy-jump -> avy-goto-word-1
-    C-u my/avy-jump -> avy-goto-char-1
+    C-u my/avy-jump -> avy-goto-char
 C-u C-u my/avy-jump -> avy-goto-line"
   (interactive "p")
   (let ((current-prefix-arg 1)
         (f (cl-case arg
-              (4 'avy-goto-char-1)
+              (4 'avy-goto-char)
               (16 'avy-goto-line)
               (t 'avy-goto-word-1))))
     (call-interactively f)))
