bb0c58ff8bd3131ec338696a9fde44505caa4eb1
Arman Mazdaee <arman.maz1373@gmail.com>
2016-07-21 13:47:30 +0430

    Set web-mode indents to 2 space

--- package-recipes/web-mode-recipe.el
+++ package-recipes/web-mode-recipe.el
@@ -1,10 +1,19 @@
 ;;Web mode for editing html template files
 (require 'require-package)
 
+;;web-mode
 (require-package 'web-mode)
 
 (add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(add-hook 'web-mode-hook
+          (lambda ()
+            (setq web-mode-enable-auto-pairing nil)
+            (setq web-mode-markup-indent-offset 2)
+            (setq web-mode-css-indent-offset 2)
+            (setq web-mode-code-indent-offset 2)
+            (setq web-mode-attr-indent-offset 2)))
 
 ;;emmet's support for emacs
 (require-package 'emmet-mode)
