f4695855df57051a73ddff1beb2af27d3bf04ff5
Arman Mazdaee <arman.maz1373@gmail.com>
2022-11-17 22:36:41 +0330

    Setup straight.el and use-package

---
+++ straight/versions/default.el
@@ -0,0 +1,8 @@
+(("el-get" . "ecd4347373fe3e00e42c2ddc37791d2fb6524caa")
+ ("emacsmirror-mirror" . "6942c3f94fe3d095d00977d3f021f59ea235e47f")
+ ("gnu-elpa-mirror" . "86617c5ec7c2437a3ab659162b221eb09604ef92")
+ ("melpa" . "aa0b80e1ee16d20c38593650f148783d4f93c822")
+ ("nongnu-elpa" . "fa571e31d32fa7a293a5c8d7e36f5739e9a01846")
+ ("straight.el" . "f21103d97d180fba0b6e2e86ebb0b6269219ef2c")
+ ("use-package" . "9090080b15486c3e337be254226efe7e5fde4c99"))
+:gamma
