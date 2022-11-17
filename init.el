8606108ba95dc315b603e7b3580d32b09a96a40b
Arman Mazdaee <arman.maz1373@gmail.com>
2022-11-17 23:06:46 +0330

    Keep emacs.d clean

--- init.el
+++ init.el
@@ -13,3 +13,19 @@
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
 (straight-use-package 'use-package)
+
+;; Better defaults
+(use-package emacs
+  :init
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (fset 'yes-or-no-p 'y-or-n-p)
+  (setq create-lockfiles nil))
+
+;; Keep ~/.emacs.d clean
+(use-package no-littering
+  :straight t
+  :config
+  (setq auto-save-file-name-transforms
+	`((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
+  (setq custom-file (no-littering-expand-etc-file-name "custom.el")))
